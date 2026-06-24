099105100d56c1b03e9116b249822a17430c423a
sungbeen park <psb1105a@gmail.com>
2026-06-24 11:23:06 +0900

    test:test

--- src/main/java/com/iduenduen/mtsservice/IDuenDuenApplication.java
+++ src/main/java/com/iduenduen/mtsservice/IDuenDuenApplication.java
@@ -13,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class IDuenDuenApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(IDuenDuenApplication.class, args);
 	}
 
